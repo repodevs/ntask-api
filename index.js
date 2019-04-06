0decb0fdb1c62408b92c68d7097eae95ceb4cd99
Edi Santoso <repopamor@gmail.com>
2019-04-07 00:18:47 +0700

    Use consign to arranging the loading of modules

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import consign from "consign";
+
 
 const PORT = 3000;
 const app = express();
@@ -7,22 +9,9 @@ const app = express();
 app.set("json spaces", 4);
 
 
-// API Home route
-app.get('/', (req, res) => res.json({
-    status: "NTask API Home"
-}));
-
-
-// API Tasks
-app.get('/tasks', (req, res) => {
-  res.json({
-    tasks: [
-      {title: "Buy some shoes"},
-      {title: "Fix notebook"}
-    ]
-  });
-});
-
+consign()
+  .include("routes")
+  .into(app);
 
 app.listen(PORT, () => console.log(`NTask API running at port ${PORT}`));
 
---
+++ routes/tasks.js
@@ -0,0 +1,11 @@
+module.exports = app => {
+  app.get("/tasks", (req, res) => {
+    res.json({
+      tasks: [
+        {title: "Buy some shoes"},
+        {title: "Fix Notebook"},
+        {title: "Learn Nodejs"}
+      ]
+    });
+  });
+};
