8cc8c1e8bcf92dd2bf3020226f906039b7cb0379
Edi Santoso <repodevs@gmail.com>
2019-04-11 15:16:50 +0700

    CRUDify: find, update, delete

--- routes/tasks.js
+++ routes/tasks.js
@@ -31,4 +31,37 @@ module.exports = app => {
         });
     })
     ;
+
+  app.route("/tasks/:id")
+    .all((req, res, next) => {
+      delete req.body.id;
+      next();
+    })
+    .get((req, res) => {
+      Tasks.findOne({where: req.params})
+        .then(result => {
+          if (result) {
+            res.json(result);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .put((req, res) => {
+      Tasks.update(req.body, {where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .delete((req, res) => {
+      Tasks.destroy({where: req.params.id})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    });
 };
